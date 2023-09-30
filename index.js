dc3986bf049482a5b8409fe282a981d3f627627b
abubakar471 <abdurjoy2001@gmail.com>
2023-09-30 12:34:51 +0600

    added hook

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const {Webhook} =require("svix");
-// import {WebhookEvent} from "@clerk/clerk-sdk-node"
+import express from "express"
+import bodyParser from "body-parser"
+import { Webhook,WebhookRequiredHeaders } from 'svix';
+import {WebhookEvent} from "@clerk/clerk-sdk-node"
 const app = express();
 
-app.post('/webhook', bodyParser.raw({type: 'application/json'}), async function(req, res) {
+app.post('/webhook', bodyParser.raw({type: 'application/json'}), function(req, res) {
+    // try {
+    //     const payload = req.body;
+    //   var headers = req.headers;
+ 
+    //     const wh = new Webhook("whsec_cuQA8EFMSSsZ7mBm3leqT/VtR0mY7aiX");
+    //     const evt = wh.verify(req.body, headers) as WebhookEvent;
+    //     const { id } = evt.data;
+    //     // Handle the webhook
+    //     const eventType = evt.type;
+    //     if (eventType === "user.created") {
+    //       console.log(`User ${id} was ${eventType}`);
+    //     }
+    //     console.log(evt.data);
+    //      res.status(200).json({
+    //         success: true,
+    //         message: 'Webhook received'
+    //      })
+    // } catch (err) {
+    //     res.status(400).json({
+    //         success: false,
+    //         message: err.message
+    //     })
+    // }
+
+    const payload = req.body;
+    const headers = req.headers;
+    const secret = "whsec_cuQA8EFMSSsZ7mBm3leqT/VtR0mY7aiX";
+
+    const wh = new Webhook(secret);
+    let msg;
+    
     try {
-        const payload = req.body;
-        const headers = req.headers;
-        console.log(req.body);
-        const wh = new Webhook("whsec_cuQA8EFMSSsZ7mBm3leqT/VtR0mY7aiX");
-        const evt = wh.verify(payload, headers);
-        const { id } = evt.data;
-        // Handle the webhook
-        const eventType = evt.type;
-        if (eventType === "user.created") {
-          console.log(`User ${id} was ${eventType}`);
-        }
-        console.log(evt.data);
-        res.status(200).json({
-            success: true,
-            message: 'Webhook received'
-         })
+        msg = wh.verify(payload, headers);
+        console.log(msg);
+        res.json({
+            "success" : true
+        });
     } catch (err) {
-        res.status(400).json({
-            success: false,
-            message: err.message
-        })
+        res.status(400).json({});
     }
 })
+
+
 
 app.listen(8000,() => {
     console.log("server is running....");
